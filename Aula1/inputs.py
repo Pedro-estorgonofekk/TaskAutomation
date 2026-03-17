a60160dd4cec498d71d9dcfe501981cc7c4df1da
unknown <pedroalvarenga173@gmail.com>
2026-03-17 20:19:44 -0300

    feat(Aula1): Calculate the liquid salary using float inputs

---
+++ Aula1/inputs.py
@@ -0,0 +1,8 @@
+#Aula de inputs 3° ano ja, é mole?
+
+salario = float(input("Salario: "))
+
+inss = salario * 0.075
+fgts = salario * 0.11
+
+print(f"Desconto INSS: {inss} \nDesconto FGTS: {fgts} \nSalario liquido: {salario - (fgts + inss)}")
