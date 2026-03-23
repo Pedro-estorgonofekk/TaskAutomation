ba333968d015a95430f41c663d72bcc568bfcf23
Pedro-estorgonofekk <pedroalvarenga173@gmail.com>
2026-03-23 07:51:54 -0300

    feat: Estrutura de repetição

---
+++ Aula3/for1.py
@@ -0,0 +1,14 @@
+##Estrutura de Repetição for
+
+for i in range(1, 10, 1):
+    print(i, end=" ")
+
+print("\n")
+
+for i in range(1, 7, 2):
+    print(i, end=" ")
+
+print("\n")
+
+for i in range(10):
+    print(i, end=" ")
