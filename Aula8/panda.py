1e3fadf4356946e6afa13493603bdf543fbe9396
Pedro-estorgonofekk <pedroalvarenga173@gmail.com>
2026-05-18 08:44:56 -0300

    feat: pandas

--- Aula8/panda.py
+++ Aula8/panda.py
@@ -2,4 +2,8 @@ import pandas as pd
 
 planilha = pd.read_excel('pranias\\Alunos.xlsx')
 
+planilha.loc[5, ["Nome", "Idade"]]
+planilha.loc[5, ["Nome", "Idade"]] = ["Ronaldo", 18]
+
+planilha.loc[len(planilha), ["Nome", "Idade", "Gênero"]] = ["Pedro", 17, "M"]
 print(planilha)
